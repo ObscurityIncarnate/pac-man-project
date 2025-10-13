cf7b9cb3d5ee3009a1d7353da642633a2a984a47
Tobi <obscurityincarnate@gmail.com>
2025-10-13 22:24:37 +0100

    Added sprite, and required elements to respective positions, fixed double border corners Todo: Need to probably remove 2 rows from the grid. Need to add in pacman and implement movement. Ghost logic. Scoring. Ghost Freightened / empowered pacman

--- js/script.js
+++ js/script.js
@@ -10,31 +10,30 @@ let userName;
 let score;
 let currentboard=0;
 let pacman = {
-	xPosition: null,
-	yPosition: null,
+	position: null,
 	lives: null,
 	lastdirection: null,
 }
 let pinkghost = {
-	xPosition: null,
-	yPosition: null,
+	position: null,
+	spawn: null,
 };
-let blueghostposition = {
-	xPosition: null,
-	yPosition: null,
+let blueghost = {
+	position: null,
+	spawn: null,
 };
-let redghostposition = {
-	xPosition: null,
-	yPosition: null,
+let redghost = {
+	position: null,
+	spawn: null,
 };
-let orangeghostposition ={
-	xPosition: null,
-	yPosition: null,
+let orangeghost ={
+	position: null,
+	spawn: null,
 };
 let gameRun;
 let timeelapsed;
 let pixels = [];
-
+let imageLocation = "url('../assets/Pac-Man - All Assets_Palettes.png')";
 //Cached Document Elements
 const boardElement = document.querySelector("#display-board");
 const mainElement = document.querySelector("main");
@@ -43,7 +42,7 @@ const prevBoardElem =  document.querySelector("#previousBoard");
 const playBoardElem =  document.querySelector("#playBoard")
 //Functions
 const setTiles= ()=>{
-for(let i=0; i<812; i++){
+for(let i=0; i<868; i++){
     const tile = document.createElement("div");
     tile.id= `tile${i}`;
     tile.classList.add("tile");
@@ -64,19 +63,22 @@ const boardSelector = (board)=>{
 const boardLayout1 = ()=>{
 	const tiles = document.querySelectorAll(".tile");
 	//verticalDoubleLinesFlexEnd 
-	const vDLFE = pixels.filter((pixel) =>{
-		return (pixel%28===0 ||[58, 86, 114, ].includes(pixel)) && ![308, 336, 364, 392, 448, 476 ].includes(pixel);
-	} );
-	const vDLFS = pixels.filter((pixel) =>{
-		return pixel%28===27 && ![335, 363, 391, 419,447, 475, 503, 531].includes(pixel)
-	} );
-	const hDLFE = pixels.filter((pixel) =>{
-		return pixel <28 ;
-	})
-	const hDLFS =  pixels.filter((pixel)=>{
-		return pixel>784;
-	})
-	const horizontalDoubleLines =[
+	const  verticalDoubleLines = pixels.filter((pixel) =>{
+		return (pixel%28===0 ||pixel%28===27 ||[58, 86, 114, ].includes(pixel)) && ![308, 336, 364, 392, 448, 476, 335, 363, 391, 419,420,447, 475, 503, 531].includes(pixel);
+	} ).concat([
+		// 313, 341, 369, 330, 358, 386, 453, 481, 509, 498, 526,
+		285, 313, 341, 302, 330, 358, 453, 481, 470, 498, 509,526,
+
+		//ghost spawn rectangle
+		374,381,
+		402, 409,
+		430, 437,
+		// 458, 465,
+		// 397,425,453,414,442,470,537,565,593,582,610
+	]);
+	const horizontalDoubleLines = pixels.filter((pixel) =>{
+		return pixel <28 ||pixel>840;
+	}).concat([
 		// 281, 282, 283, 284,
 		253, 254, 255, 256, 
 		// 303, 304, 305, 306,
@@ -86,9 +88,12 @@ const boardLayout1 = ()=>{
 		364, 365, 366, 367, 368, 386, 387, 388, 389, 390, 391, 420, 421, 422, 423, 424, 443, 444, 445, 446, 447, 
 		533, 534, 535, 536, 554, 555, 556, 557, 558,
 		//ghostSpawnRectangle
-		375, 376, 377,378, 379,380,
-		459, 460,461, 462, 463, 464,
-	]
+		375, 376, , 379,380,
+		431, 432, 433, 434, 435, 436,
+		// 459, 460,461, 462, 463, 464,
+
+		
+	])
 
 	// [ 393, 394, 395, 396, 415, 416, 417, 418, 476, 477, 478, 479, 480, 498, 499, 500, 501, 502, 503, 532, 533, 534, 535, 536, 555, 556, 557, 558, 559, 617, 618, 619, 620, 638, 639, 640, 641, 642]
 	const horizontalSingleLines = [59, 60, 
@@ -104,27 +109,82 @@ const boardLayout1 = ()=>{
 		// uppper central t
 		179,180, 181,182,183,184,
 		207, 208, , 211, 212,
-
+		
 		//left upper t
 		261,262,289, 290, 
 		//right upper t
 		269,270,297, 298,
 		//right edge smaller box
 		191, 192, 219, 220,
-	]
-	console.log(vDLFE);
-	const verticalDoubleLines = [
-		// 313, 341, 369, 330, 358, 386, 453, 481, 509, 498, 526,
-		285, 313, 341, 302, 330, 358, 425, 453, 481, 470, 498, 509,526,
 
-		//ghost spawn rectangle
-		374,381,
-		402, 409,
-		430, 437,
-		458, 465,
-		// 397,425,453,414,442,470,537,565,593,582,610
+		//under ghost spawn upper t
+		515,516,517, 518, 519, 520,
+		543, 544,, 547, 548,
+
+		// left of middle t small rectangle
+		596, 597, 598, 
+		624, 625, 626,
+		// right of middle t small rectangle
+		605, 606, 607, 
+		633, 634, 635,
+		// L right of middle t and right of small rectangle
+		611, 612,
+		640,
+		//l left of middle t and left of small rectangle
+		591,592,
+		619,
+
+		//bump on the far left 
+		// 645, 646, 
+		673, 674, 
+		701,702,
+		//bump on the far right
+		// 669, 670, 
+		697,698,
+		725, 726,
+		//bottom left assymetrical onject under left l
+		// 731,732,733, 734,
+		// 737,738,
+		// 759,760,761,762, 763, 764, 764, 765, 766 
+		759, 760, 761, 762, 
+		765, 766,
+		787, 788, 789, 790, 791, 792, 792, 793, 794,
+		//bottom t
+		683,684,685,686,687, 688,
+		711,712,715,716,
+		//bottom right asymmetric
+		773,774, 
+		777,778,779,780,
+		801, 802, 803, 804, 805, 806, 807, 808
+	]
+	 
+	const emptyTiles = [
+		//left
+		87, 88,
+		92,93, 94,
+
+		280, 281, 282,283,284,
+		308, 309, 310, 311, 312,
+		336, 337, 338, 339, 340, 
+		448, 449, 450, 451, 452, 
+		476,477,478, 479,480,
+		504,505, 506,507, 508,
+
+		//ghostcage
+		377,378, 
+		403,408,
+
+		//right
+		101, 102, 103,
+		107,108,
+		303, 304, 305, 306,307,
+		331, 332, 333, 334,335, 
+		359, 360,361,362, 363, 
+
+		471, 472, 473, 474, 475,
+		499, 500,501, 502,503, 
+		527,528,529,530,531
 	];
-	// const notVerticalDoubleLines = [447,475,476,503,532,559,560,587,588,615];
 	const verticalSingleLines =[
 		41,69,97,125,
 		42,70,98,126,
@@ -143,52 +203,177 @@ const boardLayout1 = ()=>{
 		206, 213,
 		209, 210,237,238,265,266,293,294,
 		//left T after center
-		175, 176, 203, 204,231,232, 259, 260,263,287, 288, 291,315, 316,343,344,
+		175, 176, 203, 204,231,232, 259, 260,263,287, 288, 291,315, 316,343,344,371,372,
 			// right T after center
-		187, 188 ,215,216,243,244, 268,272,296, 300,327,328, 355,356,
+		187, 188 ,215,216,243,244, 268,272,296, 300,327,328, 355,356 ,383,384,
 		// rightedge smaller box
 		190, 218, 193, 221,
 
-		
+		// in between the left hallway and right to ghost spawn
+		427,428,
+		455, 456,
+		483, 484,
+		511, 512,
+		539, 540,
+
+		// in between, lef to ghost spawn and right hallway
+		439, 440,
+		467, 468,
+		495, 496,
+		523, 524,
+		551, 552,
+
+		//middle t under ghost spawn
+		514, 521,
+		542, 549,
+		545, 546,
+		573, 574,
+		601, 602,
+		629, 630,
+
+		// left of middle t small rectangle
+		595, 599,
+		623, 627,
+
+		// right of middle t small rectangle
+		604, 608,
+		632, 636,
+
+		// L right of middle t and right of small rectangle
+		610, 613,
+		638, 639,
+		666, 667,
+		694, 695,
+		//l left of middle t and left of small rectangle
+		590,593,
+		620,621,
+		648,649,
+		676,677,
+
+		//bottom left assymetrical onject under left l
+		// 730,739,
+		// 679, 680,
+		// 707, 708,
+		// 758,767,
+		758, 767,
+		707, 708,
+		735, 736,
+		786, 795,
+		//bottom t under ghost spawn and other t
+		// 654, 661, 682, 689, 685, 686, 713, 714, 741, 742, 769, 770
+		682, 689, 710, 717, 713, 714, 741, 742, 769, 770, 797, 798,
+		// bottom right asymmetric t
+		719, 720,
+		747, 748,
+		772, 781,
+		800, 809
+
 	];
+	const leftCornerDouble =[0, 274, 442, 532];
+	const rightCornerDouble  = [27, 257, 425, 559];
+	const bottomLeftCornerDouble  = [252, 386, 554, 840];
+	const bottomRightCornerDouble  = [279, 369, 537, 867];
 	tiles.forEach((tile, index)=>{
 		// console.log(index);
 		tile.removeAttribute("class");
 		tile.classList.add("tile");
-		// if(index<28||index%28==0 || index%28 ===27 || index>=980 ||(index>= 393 && index<=396)||( index=== 397||index=== 425 ||index === 453 )||(index >=477 && index <=481)||(index >= 500 && index <=502 )||( index>= 533 && index<=535 )||( index<= 558 && index >=556)){
 			//looping paths
-			if(!(index===420||index===448 ||index==504 || index==504+27)){
+			if(!(emptyTiles.includes(index))){
 				const divider = document.createElement("div");
 				//vertical dividers
-				
-				if(vDLFE.includes(index) || vDLFS.includes(index) || verticalDoubleLines.includes(index) ||verticalSingleLines.includes(index)){
+				if(leftCornerDouble.includes(index) || rightCornerDouble.includes(index)|| bottomLeftCornerDouble.includes(index) || bottomRightCornerDouble.includes(index)){
+					const smallerCorner =document.createElement("div");
+					console.log("here")
+					
+					if(leftCornerDouble.includes(index)){
+						divider.style.borderTop = "1px solid blue";
+						smallerCorner.style.borderTop = "1px solid blue";
+						divider.style.borderLeft ="1px solid blue";
+						smallerCorner.style.borderLeft ="1px solid blue";
+						tile.style.justifyContent = "flex-end";
+						tile.style.alignItems= "flex-end";
+						divider.style.borderTopLeftRadius = "3px";
+						smallerCorner.style.height="5px";
+						smallerCorner.style.width = "6px";
+						divider.style.height = "9px";
+						divider.style.width ="10px";
+						smallerCorner.style.borderTopLeftRadius = "3px";
+					}else if(rightCornerDouble.includes(index)){
+						divider.style.borderTop = "1px solid blue";
+						smallerCorner.style.borderTop = "1px solid blue";
+						divider.style.borderRight ="1px solid blue";
+						smallerCorner.style.borderRight = "1px solid blue";
+						divider.style.borderTopRightRadius = "3px";
+						smallerCorner.style.borderTopRightRadius = "3px";
+						smallerCorner.style.height="5px";
+						smallerCorner.style.width = "5px"
+						divider.style.height = "9px";
+						divider.style.width ="9px";
+						tile.style.justifyContent= "flex-start";
+						tile.style.alignItems= "flex-end";
+					}else if(bottomLeftCornerDouble.includes(index)){
+						divider.style.borderBottom = "1px solid blue";
+						smallerCorner.style.borderBottom = "1px solid blue";
+						divider.style.borderLeft ="1px solid blue";
+						smallerCorner.style.borderLeft ="1px solid blue";
+						smallerCorner.style.height="6px";
+						smallerCorner.style.width = "6px";
+						divider.style.height = "10px";
+						divider.style.width ="10px";
+						divider.style.borderBottomLeftRadius = "3px";
+						smallerCorner.style.borderBottomLeftRadius = "3px";
+						tile.style.alignItems= "flex-start";
+						tile.style.justifyContent = "flex-end";
+					}else{
+						divider.style.borderBottom = "1px solid blue";
+						smallerCorner.style.borderBottom = "1px solid blue";
+						divider.style.borderRight ="1px solid blue";
+						smallerCorner.style.borderRight = "1px solid blue";
+						divider.style.borderBottomRightRadius = "3px";
+						smallerCorner.style.borderBottomRightRadius = "3px";
+						smallerCorner.style.height="6px";
+						smallerCorner.style.width = "5px"
+						divider.style.height = "10px";
+						divider.style.width ="9px";
+						tile.style.justifyContent= "flex-start";
+						tile.style.alignItems= "flex-start";
+					}
+					
+					
+					
+					
+					// divider.style.borderRight = "1px solid blue";
+					// divider.style.borderTopLeftRadius = "100%";
+					// divider.style.borderTopRightRadius= "-100%"
+					// divider.style.transform= "translate(-50%, -50%)";
+					// divider.style.borderRadius = "2px"
+					
+					divider.style.zIndex = "0"
+					
+					smallerCorner.style.zIndex = "1"
+					// divider.style.width = "3px";
+					tile.appendChild(divider);
+					divider.style.position = "absolute";
+					tile.appendChild(smallerCorner);
+					tile.classList.add("blocked");
+					
+					
+				}else if(verticalDoubleLines.includes(index) ||verticalSingleLines.includes(index)){
 					if(verticalSingleLines.includes(index)){
 						divider.style.borderLeft = "1px solid blue";
-						// divider.style.borderRight = "1px solid blue";
 						tile.style.justifyContent= "center"
 						divider.style.height ="inherit";
-						// divider.style.width = "10px";
 					}else {
-						// if(vDLFE.includes(index)){
-						// 	tile.style.justifyContent ="flex-End";
-						// }
 						divider.style.borderLeft = "1px solid blue";
 						divider.style.borderRight = "1px solid blue";
 						divider.style.height ="inherit";
 						divider.style.width = "3px";
 					}
-					
-				}else if(hDLFE.includes(index)||hDLFS.includes(index)|| horizontalDoubleLines.includes(index) || horizontalSingleLines.includes(index)){
-					// horizontalDoubleLines.push(index);
-					// console.log(index);
-					//horizontal dividers
-					// if(vDLFE.includes(index)){
-					// 	tile.style.alignItems = "flex-end"
-					// }
+					tile.appendChild(divider)
+					tile.classList.add("blocked");
+				}else if( horizontalDoubleLines.includes(index) || horizontalSingleLines.includes(index)){
 					if(horizontalSingleLines.includes(index)){
 						divider.style.borderTop="1px solid blue";
-					// divider.style.borderBottom ="1px solid blue";
-					// divider.style.height = "3px";
 						divider.style.width = "inherit";
 					}else{
 						divider.style.borderTop="1px solid blue";
@@ -196,30 +381,41 @@ const boardLayout1 = ()=>{
 						divider.style.height = "3px";
 						divider.style.width = "inherit";
 					}
-					
-
-					// tile.style.alignItems ="center";
+					tile.appendChild(divider)
+					tile.classList.add("blocked");
+				}
+				else if(index === 85 ||index ===110|| index === 645 || index === 669){
+					tile.classList.add("powerPellet");
+				}else if(index == 404){
+					pinkghost.spawn = 404;
+					pinkghost.position = 404;
+					tile.classList.add("pinkGhost")
+				}else if(index ==405){
+					blueghost.spawn =405;
+					blueghost.position =405;
+					tile.classList.add("blueGhost")
+				}else if(index == 406){
+					redghost.spawn = 406;
+					redghost.position = 406;
+					tile.classList.add("redGhost")
+					// ti
+				}else if(index == 407){
+					orangeghost.spawn = 407;
+					orangeghost.position =  407;
+					tile.classList.add("orangeGhost")
 				}else{
-
+						const dot = document.createElement("div");
+						tile.classList.add("dot")
+						tile.appendChild(dot);
 				}
-				if(index ===421){
-					tile.style.backgroundColor = "white";
-				}
-				// divider.style.borderLeft = "1px solid blue";
-				// divider.style.borderRight = "1px solid blue";
-				// divider.style.length ="inherit";
-				// divider.style.width = "3px"
-				tile.appendChild(divider)
-				tile.classList.add("blocked");
 			}
 			
 		// }
 	});
-	// console.log(horizontalDoubleLines.)
 }
 
 setTiles();
-
+boardSelector(0);
 //Eventlistener
 nextBoardElem.addEventListener("click", ()=>{
 	currentboard = (currentboard+1)%boardAmount;
@@ -230,6 +426,8 @@ prevBoardElem.addEventListener("click", ()=>{
 
 	boardSelector(currentboard);
 });
+playBoardElem.addEventListener("click", ()=>{
 
+})
 
 // console.log(boardElement);
